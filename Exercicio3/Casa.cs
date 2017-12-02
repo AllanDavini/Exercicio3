26d9c004f381deb4d3b801367879be67abb2a536
AllanDavini <allan_mv19@hotmail.com>
2017-12-02 02:27:35 -0200

    inicio

---
+++ Exercicio3/Casa.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Exercicio3
+{
+    public class Casa
+    {
+        public Casa(Rua rua, int numero, int totalDeCriancas)
+        {
+            Rua = rua;
+            Numero = numero;
+            TotalDeCriancas = totalDeCriancas;
+        }
+
+        public Rua Rua { get; set; }
+        public int Numero { get; set; }
+        public int TotalDeCriancas { get; set; }
+    }
+}
---
+++ Exercicio3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Rua> rua = new List<Rua>
+            {
+                new Rua{Cep = "03282001", Nome = "Rua do Oratorio"},
+                new Rua{Cep = "03282002", Nome = "Rua da Mooca"},
+                new Rua{Cep = "03282003", Nome = "Avenida Dom Pedro"},
+                new Rua{Cep = "03282004", Nome = "Avenida Ibirapuera"},
+                new Rua{Cep = "03282005", Nome = "Avenida Ayrton Senna"},
+            };
+            
+            Dictionary<Casa, Rua> vacina = new Dictionary<Casa, Rua>();
+        }
+    }
+}
